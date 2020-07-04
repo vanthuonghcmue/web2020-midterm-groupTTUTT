b8108c771c7767ad1917c8fa3a4d91bac01f447b
thuanle2409 <thuanle24092000@gmail.com>
2020-07-04 09:41:56 +0700

    chèn nội dung

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    
+    <title>Nội thất| Ngoại thất và Decor| Hàng xuất khẩu cao cấp</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./vendors/fonts/css/fontawesome.css">
+    <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/redirect?Id=CvmsVv9asuSiOaRBBz9urpS909ixNzNqI37%2b8O1yQr4%3d">
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@700&family=Quicksand:wght@500&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="resources/css/styles.css">
+    <link rel="stylesheet" href="./vendors/css/bootstrap.css">
+    <link rel="stylesheet" href="./resources/css/queries.css">
+    <link rel="stylesheet" href="./vendors/css/bootstrap-grid.css">
+    <script src="./vendors/js/jquery-3.5.0.js"></script>
+    <script src="https://kit.fontawesome.com/973a1060ee.js" crossorigin="anonymous"></script>
+
+
+</head>
+
+<body>
+
+    <header id="Home">
+        <!-- Begin head  -->
+        <nav>
+
+            <div>
+                <ul class="main-nav">
+                    <li><a class="a" href="#">Nội thất</a>
+                        <ul class="submenu">
+                            <li><a href=""> SoFa</a></li>
+                            <li><a href=""> Bàn </a></li>
+                            <li><a href=""> Ghế </a></li>
+                            <li><a href=""> Tủ/Kệ </a></li>
+                            <li><a href=""> Phụ Kiện </a></li>
+                        </ul>
+                    </li>
+                    <li><a class="a" href="#">Ngoại Thất</a>
+                        <ul class="submenu">
+                            <li><a href=""> Sofa ngoài trời </a></li>
+                            <li><a href=""> Bàn ngoài trời </a></li>
+                            <li><a href=""> Ghế ngoài trời </a></li>
+                            <li><a href=""> Bộ bàn ghế ngoài trời </a></li>
+                            <li><a href=""> Ghế hồ bơi </a></li>
+                            <li><a href=""> Ngoại thất khác </a></li>
+                            <li><a href=""> Phụ kiện </a></li>
+                        </ul>
+                    </li>
+                    <li><a class="a" href="#">Decor</a>
+                        <ul class="submenu">
+                            <li><a href=""> Bình/Lọ </a></li>
+                            <li><a href=""> Tượng </a></li>
+                            <li><a href=""> Lighting </a></li>
+                            <li><a href=""> Đài phun nước </a></li>
+                            <li><a href=""> Accessaries </a></li>
+                        </ul>
+                    </li>
+                    <li><a class="a" href="#">Sân Vườn</a>
+                        <ul class="submenu">
+                            <li><a href=""> Chậu cây </a></li>
+                            <li><a href=""> Decord sân vườn </a></li>
+                            <li><a href=""> Thác nước </a></li>
+                            <li><a href=""> Lavabo </a></li>
+                            <li><a href=""> Phụ kiện vườn </a></li>
+                        </ul>
+                    </li>
+                    <li><a class="a" href="#">Đồ gia dụng</a>
+                        <ul class="submenu">
+                            <li><a href=""> Phòng tắm </a></li>
+                            <li><a href=""> Make-up </a></li>
+                            <li><a href=""> Ngăn/Kệ </a></li>
+                        </ul>
+                    </li>
+                    <li><a class="a" href="#">Thương hiệu</a>
+                        <ul class="submenu">
+                            <li><a href=""> Nhata </a></li>
+                            <li><a href=""> 4seas </a></li>
+                            <li><a href=""> LifestyleGarden </a></li>
+                            <li><a href=""> Estares </a></li>
+                            <li><a href=""> Segis </a></li>
+                            <li><a href=""> Cellfast </a></li>
+                            <li><a href=""> Cerubo </a></li>
+                        </ul>
+                    </li>
+                    <a class="fas fa-shopping-cart " href="#" id="icoi"></a>
+                    <a class="far fa-user " href="./pages/login2.html" id="icoi"></a>
+                </ul>
+            </div>
+            <div class="mobile-nav-icon"><i class="fa fa-bars "></i></div>
+        </nav>
+
+        <div class="clearfix"> </div>
+        <div class="ROW">
+            <div class="heading-main-box">
+                <h>
+                    tiện nghi, sang trọng, <br>
+                    đẳng cấp và tinh tế
+                </h>
+            </div>
+        </div>
+        <!-- end head -->
+    </header>
+
+    <section class="About-section row ">
+        <!-- Bắt đầu phần giới thiệu sản phẩm mới -->
+        <div class="bosuutap col span_1_of_2">
+            <p class="tieude">2020 Collection<br></p>
+            <p class="gioithieu">
+                Bộ sưu tập các sản phẩm ngoài trời lớn nhất Việt Nam.-
+                Được sản xuất với công nghệ và chất lượng tốt nhất, phù hợp sử dụng trong mọi điều kiện thời tiết;
+                từ ban công có mái che, khu vườn vùng nhiệt đới, khu vực Bắc Âu lạnh giá khắc nghiệt
+                cho tới những vùng biển Địa Trung Hải nắng nóng,
+                các sản phẩm ngoài trời của chúng tôi đều thích hợp sử dụng
+            </p>
+        </div>
+       
+
+        <div id="slider" class="gioithieuimg col span-1_of_2 popup-gallery">
+            <a href="./resources/img/hình trang chủ/ghế 2.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="slide" src="./resources/img/hình trang chủ/ghế 2.webp" alt="photo" stt="1"
+                    style="display:none"></a>
+            <a href="./resources/img/hình trang chủ/ghế 1.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="slide" src="./resources/img/hình trang chủ/ghế 1.webp" alt="photo" stt="0"></a>
+            <a href="./resources/img/hình trang chủ/ghế 3.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="slide" src="./resources/img/hình trang chủ/ghế 3.webp" alt="photo" stt="2"
+                    style="display:none"></a>
+            <a href="./resources/img/hình trang chủ/ghế 4.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="slide" src="./resources/img/hình trang chủ/ghế 4.webp" alt="photo" stt="3"
+                    style="display:none"></a>
+            <a href="./resources/img/hình trang chủ/ghế 5.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="slide" src="./resources/img/hình trang chủ/ghế 5.webp" alt="photo" stt="4"
+                    style="display:none"></a>
+            <a href="./resources/img/hình trang chủ/ghế 6.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="slide" src="./resources/img/hình trang chủ/ghế 6.webp" alt="photo" stt="5"
+                    style="display:none"></a>
+            <a href="./resources/img/hình trang chủ/ghế 7.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="slide" src="./resources/img/hình trang chủ/ghế 7.webp" alt="photo" stt="6"
+                    style="display:none"></a>
+            <a href="./resources/img/hình trang chủ/ghế 8.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="slide" src="./resources/img/hình trang chủ/ghế 8.webp" alt="photo" stt="7"
+                    style="display:none"></a>
+        </div>
+         <!-- kết thúc phần giới thiệu sản phẩm mới -->
+    </section>
+
+    <section class="products-section container-fluid ">
+        <!-- Bắt đầu phần phân loại sản phẩm -->
+        <div class="row colum">
+            <div class="col md-6 about-picture">
+
+                <a href="./pages/show-row-Ngoại_thất.html"><img src="./resources/img/1.webp" alt="photo"></a>
+                <p class="picture-title"> Ngoại Thất</p>
+            </div>
+            <div class="col md-6 about-picture">
+
+                <a href="#"> <img style="height: 100%;" src="./resources/img/soddfa.webp" alt="photo"></a>
+                <p href="#" class="picture-title"> Sofa</p>
+            </div>
+        </div>
+
+        <div class="row colum">
+            <div class="col md about-picture">
+
+                <a href="#"> <img src="./resources/img/chaucay.webp" alt="photo"></a>
+                <p class="picture-title"> Chậu Cây</p>
+            </div>
+            <div class="col md-4 about-picture">
+
+                <a href="#"> <img src="./resources/img/banghe.webp" alt="photo"></a>
+                <p class="picture-title"> Bàn ghế</p>
+            </div>
+            <div class="col md-4 about-picture">
+
+                <a href="#"> <img src="./resources/img/decor.webp" alt="photo"></a>
+                <p class="picture-title"> Trang trí</p>
+            </div>
+        </div>
+        <!-- Kết thúc phân loại sản phẩm -->
+    </section>
+
+    <section id="water" class="advertisement-section row">
+        <!-- Bắt đầu giới thiệu về thương hiệu -->
+        <div class="col md-6 popup-gallery2 ">
+            <a href="./resources/img/gt.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="advertisement-section-img" src="./resources/img/gt.webp" alt="poto" st="0"> </a>
+            <a href="./resources/img/hình trang chủ/thác nước 1.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="advertisement-section-img" src="./resources/img/hình trang chủ/thác nước 1.webp" alt="poto"
+                    st="1" style="display:none"> </a>
+            <a href="./resources/img/hình trang chủ/thác nước 2.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="advertisement-section-img" src="./resources/img/hình trang chủ/thác nước 2.webp" alt="poto"
+                    st="2" style="display:none"> </a>
+            <a href="./resources/img/hình trang chủ/thác nước 3.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="advertisement-section-img" src="./resources/img/hình trang chủ/thác nước 3.webp" alt="poto"
+                    st="3" style="display:none"> </a>
+            <a href="./resources/img/hình trang chủ/thác nước 4.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="advertisement-section-img" src="./resources/img/hình trang chủ/thác nước 4.webp" alt="poto"
+                    st="4" style="display:none"> </a>
+            <a href="./resources/img/hình trang chủ/thác nước 5.webp" title="sản phẩm mới nhất của bộ sưu tập"><img
+                    class="advertisement-section-img" src="./resources/img/hình trang chủ/thác nước 5.webp" alt="poto"
+                    st="5" style="display:none"> </a>
+
+        </div>
+
+
+    <footer>
+        <!-- Begin footer -->
+        <p>
+            Copyright &copy; 2020 by TTUTT TEAM
+        </p>
+        <!-- end footer -->
+    </footer>
+
+</body>
+
+</html>
+
+
+
