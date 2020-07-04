29682da72f4dc9783c773a1069eacbd10245ecd9
vanthuonghcmue <47852060+vanthuonghcmue@users.noreply.github.com>
2020-07-04 08:54:47 +0700

    ket noi co so du lieu

--- libraries/Database.php
+++ libraries/Database.php
@@ -4,7 +4,7 @@ class DataProvider
 	public static function ExecuteQuery($sql)
 	{
 		try{
-			$connection = mysqli_connect("localhost","root","", "webnoithat");//or die ("couldn't connect to localhost");				
+			$connection = mysqli_connect("localhost","root","", "giuaki");//or die ("couldn't connect to localhost");				
 			mysqli_query($connection, "set names 'utf8'");		
 			$result = mysqli_query($connection, $sql);		
 			mysqli_close($connection);		
