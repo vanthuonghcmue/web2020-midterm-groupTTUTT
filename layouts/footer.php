502b524bb8b4c9328aadbd3830ff0f6a6694817d
vanthuonghcmue <47852060+vanthuonghcmue@users.noreply.github.com>
2020-07-04 11:55:26 +0700

    nop bai

--- layouts/footer.php
+++
@@ -1,9 +0,0 @@
-<footer>
-        <!-- Begin footer -->
-        <p>
-            Copyright &copy; 2020 by TTUTT TEAM
-        </p>
-        <!-- end footer -->
-    </footer>
-</body>
-<!-- end head -->
